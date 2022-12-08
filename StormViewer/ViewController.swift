e53a42ccce58b68179d13b3da7a2c8af255a09ea
mfsaglam <mfsaglam1@gmail.com>
2022-12-08 22:02:58 +0300

    make the test pass

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -13,9 +13,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addImages()
     }
+    
+    func addImages() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
 
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+    }
 
 }
 
