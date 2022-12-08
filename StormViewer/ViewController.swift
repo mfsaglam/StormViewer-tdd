7bcce1ab6bea01165a6c31093dce0037353b515c
mfsaglam <mfsaglam1@gmail.com>
2022-12-08 18:14:49 +0300

    first commit

---
+++ StormViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Fatih Sağlam on 8.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
