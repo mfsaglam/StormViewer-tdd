2ef7d54aac39069a784873fe06671bcba2c2815b
mfsaglam <mfsaglam1@gmail.com>
2022-12-11 19:13:01 +0300

    refactor tests

--- StormViewerTests/DetailViewTests.swift
+++ StormViewerTests/DetailViewTests.swift
@@ -58,53 +58,36 @@ final class DetailViewTests: XCTestCase {
     func testSelectingImageShowsDetail() {
         // given
         let sut = ViewController()
-        let navigationController = UINavigationController(rootViewController: sut)
+        var selectedImage: String?
         let testIndexPath = IndexPath(row: 0, section: 0)
 
         // when
+        sut.pictureSelectAction = {
+            selectedImage = $0
+        }
+
         sut.tableView(sut.tableView, didSelectRowAt: testIndexPath)
 
-        // create an expectation…
-        let expectation = XCTestExpectation(description: "Selecting a table view cell.")
-        
-        // …then fulfill it asynchronously
-        DispatchQueue.main.async {
-            expectation.fulfill()
-        }
-        
         // then
-        wait(for: [expectation], timeout: 1)
-        XCTAssertTrue(navigationController.topViewController is DetailViewController)
+        XCTAssertEqual(selectedImage, "nssl0049.jpg")
     }
     
     func testSelectingImageShowsDetailImage() {
         // given
         let sut = ViewController()
-        let navigationController = UINavigationController(rootViewController: sut)
         let testIndexPath = IndexPath(row: 0, section: 0)
         let filenameToTest = "nssl0049.jpg"
         let imageToLoad = UIImage(named: filenameToTest, in: Bundle.main, compatibleWith: nil)
 
         // when
+        sut.pictureSelectAction = {
+            let detail = DetailViewController()
+            detail.selectedImage = $0
+            detail.loadViewIfNeeded()
+            XCTAssertEqual(detail.imageView.image, imageToLoad)
+        }
+
         sut.tableView(sut.tableView, didSelectRowAt: testIndexPath)
-
-        let expectation = XCTestExpectation(description: "Selecting a table view cell.")
-
-        DispatchQueue.main.async {
-            expectation.fulfill()
-        }
-
-        // then
-        wait(for: [expectation], timeout: 1)
-
-        guard let detail = navigationController.topViewController as? DetailViewController else {
-            XCTFail("Didn't push to a detail view controller.")
-            return
-        }
-
-        detail.loadViewIfNeeded()
-
-        XCTAssertEqual(detail.imageView.image, imageToLoad)
     }
 
 }
